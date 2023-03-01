e7d9008e7b5b614720d2dc3964954b4c17e786d7
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-01 17:23:22 -0300

    creates the async function getworker that guarantee it is going to work on all browsers

--- classes/class-01/pages/video-player/src/factory.js
+++ classes/class-01/pages/video-player/src/factory.js
@@ -1,8 +1,24 @@
 import Camera from "../../../lib/shared/camera.js";
+import { supportsWorkersType } from "../../../lib/shared/util.js";
 import Controller from "./controller.js";
 import Service from "./service.js";
 import View from "./view.js";
 
+async function getWorker() {
+  if (supportsWorkersType) {
+    const worker = new Worker('./src/worker.js', {type:'module'})
+    console.log("suporta");
+    return worker;
+  }
+  const workerMock = {
+    async postMessage() {},
+    onmessage(msg) {}
+  }
+  console.log("nao suporta");
+  return workerMock;
+}
+
+const worker = await getWorker();
 const camera = await Camera.init();
 const [rootPath] = window.location.href.split("/pages/");
 const factory = {
