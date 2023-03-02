9595417288a3b47910b19410f8c570a52ca443ce
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-02 17:13:07 -0300

    imports the view and service and creates their methods

--- classes/class-01/pages/video-player/src/controller.js
+++ classes/class-01/pages/video-player/src/controller.js
@@ -1,14 +1,27 @@
-export default class Controller{
-    constructor({}){
+export default class Controller {
+  #view;
+  #service;
+  constructor({ view, service }) {
+    this.#view = view;
+    this.#service = service;
+    this.#view.configureOnBtnClick(this.onBtnStart.bind(this));
+  }
 
-    }
+  static async initialize(deps) {
+    const controller = new Controller(deps);
+    controller.log("Not yet detecting eye blink. Click on the button to start");
+    return controller.init();
+  }
 
-    static async initialize(deps) {
-        const controller = new Controller(deps)
-        return controller.init()
-    }
+  async init() {
+    console.log("init!!!");
+  }
 
-    async init(){
-        console.log('init!!!')
-    }
+  log(text) {
+    this.#view.log(`logger: ${text}`);
+  }
+
+  onBtnStart() {
+    this.log("Initializing detection...");
+  }
 }
