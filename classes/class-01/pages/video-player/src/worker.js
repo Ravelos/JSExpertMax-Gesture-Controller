38b08692ac4abc339130dc282502128d401dbb68
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-15 21:16:16 -0300

    creates the methods for this worker

--- classes/class-01/pages/video-player/src/worker.js
+++ classes/class-01/pages/video-player/src/worker.js
@@ -6,21 +6,22 @@ import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist
 import Service from "./service.js";
 // in the main process is window
 // in worker is self
-const { tf, faceLandmarksDetection } = self;
-tf.setBackend("webgl");
+const {tf, faceLandmarksDetection } = self
+tf.setBackend('webgl')
 
 const service = new Service({
   faceLandmarksDetection
-});
+})
 
-console.log('Loading tf model');
+console.log('loading tf model')
 await service.loadModel()
-console.log('tf model loaded!');
+console.log('tf model loaded!')
 postMessage('READY')
 
-onmessage = ({ data }) => {
-  console.log("worker!", data);
+onmessage = async ({ data:video }) => {
+  const blinked = await service.handBlink(video)
+  if(!blinked) return;
   postMessage({
-    ok: "ok",
+    'ok': "ok",
   });
 };
